df0a7a6f50fcd3598ba8f255497da759f0ca203b
Vitor <direcaoass@gmail.com>
2023-01-13 14:18:55 -0300

    comments deleted

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@ const prevDisplay = document.querySelector("#previous-operation");
 const currentDisplay = document.querySelector("#current-operation");
 const buttons = document.querySelectorAll("#buttons-container button");
 
-class Calculadora {
+class Calculator {
   constructor() {
     this.prevDisplay = prevDisplay;
     this.currentDisplay = currentDisplay;
-    this.currentOperation = ""; // é o digito que está sendo pressionado no exato momento.
+    this.currentOperation = ""; 
   }
 
-  // adiciona um digito a tela
+  
   addDigit(digit) {
-    // Verifica se o numero digitado é um ponto, para evitar repetição.
+    
     if (digit === "." && this.currentDisplay.innerText.includes(".")) {
       return;
     }
@@ -20,17 +20,17 @@ class Calculadora {
 
   }
 
-  // Processa a operação de acordo com o botão apertado enviado no parametro operador
+  
   processOperation(operator) {
 
-    //Verifica condições para apenas mudar o sinal do operador
+    
     if (this.currentDisplay.innerText === "" && operator !== "C") {
       if (this.prevDisplay.innerText !== "") {
         this.changeOperation(operator);
       }
       return;
     }
-    //Calcula o resultado da operação
+    
     let operationResult;
     let previous = Number(this.prevDisplay.innerText.split(" ")[0]);
     let current = Number(this.currentDisplay.innerText);
@@ -69,7 +69,7 @@ class Calculadora {
     }
   }
 
-  // Muda o sinal matematico da operação no prevdisplay.
+  
   changeOperation(operator) {
     const Operations = ["*", "-", "+", "/"];
 
@@ -80,46 +80,42 @@ class Calculadora {
   }
 
 
-  // Atualiza os valores do display e da operação
-  // Esses parametros apenas recebem valor, quando o metodo processOperation for utilizado.
   updateScreen(
     operationResult = null,
     operator = null,
     current = null,
     previous = null) {
-    if (operationResult === null) { //apenas adiciona os digitos pois não tem dados para calcular resultado
+    if (operationResult === null) { 
       this.currentDisplay.innerText += this.currentOperation;
     }
-    else {// qnd é inserido um operador, mesmo sem um previous, ele vem pra cá.
+    else {
 
-      if (previous === 0) { // apenas adiciona no segundo display pois nao tem dados para calcular resultado
+      if (previous === 0) { 
         operationResult = current;
       }
 
-      //atualiza os displays com os dados obtidos no metodo processOperation
+      
       this.prevDisplay.innerText = `${operationResult} ${operator}`;
       this.currentDisplay.innerText = "";
     }
   }
 
-  // Processa resultado do botão igual
   processEqualOperator() {
     let operator = this.prevDisplay.innerText.split(" ")[1];
     this.processOperation(operator);
   }
 
-  // Limpa o display atual
   processClearCurrentOperator() {
     this.currentDisplay.innerText = "";
   }
 
-  // Limpa os 2 displays
+  
   processClearOperator() {
     this.currentDisplay.innerText = "";
     this.prevDisplay.innerText = "";
   }
 
-  // Deleta o ultimo digito
+  
   processDelOperator() {
     this.currentDisplay.innerText =
       this.currentDisplay.innerText.slice(0, -1);
@@ -127,7 +123,7 @@ class Calculadora {
 
 }
 
-const calc = new Calculadora();
+const calc = new Calculator();
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
@@ -141,7 +137,7 @@ buttons.forEach((btn) => {
   });
 });
 
-const mapaTeclado = {
+const keyboardMap = {
   '0': 'tecla0',
   '1': 'tecla1',
   '2': 'tecla2',
@@ -164,7 +160,7 @@ const mapaTeclado = {
 
 document.addEventListener('keydown', (e) => {
   const tecla = e.key
-  document.getElementById(mapaTeclado[tecla]).click();
+  document.getElementById(keyMap[tecla]).click();
 });
 
 
